a7a24a1923b9f527e199533909161d29b27dd514
sparksy92 <blair_sparks@hotmail.com>
2026-07-22 11:23:34 -0400

    chore: Update background image to mountain side homestead

--- storefront/components/home/HomesteadBackground.tsx
+++ storefront/components/home/HomesteadBackground.tsx
@@ -6,12 +6,12 @@ export default function HomesteadBackground() {
   return (
     <div className="absolute inset-0 w-full h-full overflow-hidden bg-black select-none pointer-events-none">
       <Image
-        src="/bakery_hearth.jpg"
-        alt="Cozy Bakery Hearth Background"
+        src="/mountain_homestead.jpg"
+        alt="Mountain Homestead Background"
         fill
         sizes="100vw"
         priority
-        className="object-cover object-center brightness-[0.85]"
+        className="object-cover object-center brightness-[0.80]"
       />
     </div>
   );
